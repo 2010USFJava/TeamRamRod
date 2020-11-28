3950ab6e7bbd4469f39550de3839e8b845eb143b
rajsukanya <sukanyaraj1014@yahoo.com>
2020-11-27 21:10:53 -0500

    finished admin login

--- ProReimbursement/src/main/java/com/revature/dao/ManagerDao.java
+++ ProReimbursement/src/main/java/com/revature/dao/ManagerDao.java
@@ -1,11 +1,16 @@
 package com.revature.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.revature.beans.Manager;
 
 public interface ManagerDao {
-	public void createManager(Manager m) throws SQLException;
+	public void createAdmin(Manager a) throws SQLException;
 	
-	public Manager findManagerById(int employeeID) throws SQLException;
+	public Manager findAdminById(int employeeID) throws SQLException;
+
+	public Manager getAdminByEmail(String email) throws SQLException;
+	
+	public List<Manager> getAllAdmins() throws SQLException;
 }
--- ProReimbursement/src/main/java/com/revature/servlet/RequestHelper.java
+++ ProReimbursement/src/main/java/com/revature/servlet/RequestHelper.java
@@ -2,22 +2,31 @@ package com.revature.servlet;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.revature.controller.HomeController;
+import com.revature.controller.AdminLoginController;
 import com.revature.controller.CusLoginController;
+import com.revature.controller.HomeController;
 
 public class RequestHelper {
 	
 	public static String process(HttpServletRequest req) {
 		System.out.println(req.getRequestURI()); //URI: what resource am I going to get from this domain
-		System.out.println(req.getRequestURI());
+		
 		switch(req.getRequestURI()) {
 		case "/ProReimbursement/login.customer":
-			System.out.println("in login rhelper");
+			System.out.println("in customer login rhelper");
 			return CusLoginController.login(req);
 		
 		case "/ProReimbursement/home.customer":
-			System.out.println("in home rhelper");
-			return HomeController.home(req); //home controller takes us to home.html (view back to us)
+			System.out.println("in customer home rhelper");
+			return HomeController.customerHome(req); //home controller takes us to home.html (view back to us)
+		
+		case "/ProReimbursement/login.admin":
+			System.out.println("in admin login rhelper");
+			return AdminLoginController.login(req);
+			
+		case "/ProReimbursement/home.admin":
+			System.out.println("in admin home rhelper");
+			return HomeController.adminHome(req); //home controller takes us to home.html (view back to us)
 			
 		default:
 			System.out.println("in default case");
